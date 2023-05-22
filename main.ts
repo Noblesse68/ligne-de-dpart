dcdd82b59c210176d19a1abd543248fc8e1027d6
Noblesse68 <132678684+Noblesse68@users.noreply.github.com>
2023-05-22 10:46:26 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+let timer = 0
+input.onSound(DetectedSound.Loud, function () {
+    timer = 0
+    if (input.soundLevel() < 140) {
+        while (input.soundLevel() < 140) {
+            basic.showNumber(timer)
+            basic.pause(1000)
+            timer += 0 + 1
+            continue;
+        }
+    }
+    basic.showNumber(timer)
+    basic.pause(10000)
 })
